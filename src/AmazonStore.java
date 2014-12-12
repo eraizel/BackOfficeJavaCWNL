f09ca2f062a046c6be296c7fc46820f738d5e000
Administrator <Administrator@user09>
2014-12-12 15:13:14 +0000

    Created JFrame Amazon

--- src/AmazonStore.java
+++ src/AmazonStore.java
@@ -1,4 +1,16 @@
 
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -10,9 +22,16 @@
  *
  * @author eraizel
  */
-public class AmazonStore {
-    static Products[] productsList = new Products [100];
+public class AmazonStore extends JFrame implements ActionListener {
+    JButton b1, b2, b3;
+    JPanel container, p1, p2, p3;
+    CardLayout c1;
+    JTextField t1;
+    
+    
     public static void main (String [] args){
+        new AmazonStore();
+        
         // create a new instance of a product
     Products p1 = new Products(1,"Microsoft Office",99.99,2014,"Microsoft");
     Book b1 = new Book(1,"Microsoft Office",99.99,2014,"Microsoft","Eliazer Raizel", "123123DEASD123", 365);
@@ -24,6 +43,57 @@ public class AmazonStore {
     System.out.println(s1.getSoftProcSpeed());
 
     
+    }
+    AmazonStore(){
+        super("Amazon Store");
+        setSize(500,500);
+        setLayout(new FlowLayout());
+        
+        b1 = new JButton("one");
+        b2 = new JButton("two");
+        b3 = new JButton("three");
+        add(b1);
+        add(b2);
+        add(b3);
+        
+        p1 = new JPanel();
+        p1.setBackground(Color.red);
+        p2 = new JPanel();
+        p2.setBackground(Color.blue);
+        p3 = new JPanel();
+        p3.setBackground(Color.yellow);
+        t1 = new JTextField(10);
+        p2.add(t1);
+        
+        container = new JPanel();
+        container.setPreferredSize(new Dimension(300,300));
+        container.setBackground(Color.green);
+        c1 = new CardLayout();
+        container.setLayout(c1);
+        container.add(p1, "card1");
+        container.add(p2, "card2");
+        container.add(p3, "card3");
+        add(container);
+        
+        setVisible(true);
+        
+        
+        
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == b1){
+            c1.show(container, "card1");
+        }
+        if(e.getSource() == b2){
+            c1.show(container, "card2");
+        }
+        if(e.getSource() == b3){
+            c1.show(container, "card3");
+        }
+        
     }
     
 }
